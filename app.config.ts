2aaf1943a59247f38747b3656fe47369c6447468
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-24 11:01:03 +0530

    nuxt-umami

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,10 @@ export default defineAppConfig({
   ui: {
     primary: 'lime',
     gray: 'neutral',
-  }
+  },
+  umami: {
+    version: 2,
+    host: 'https://analytics.net',
+    id: 'c8f08896-1723-4cd7-bd28-87b180d37xx2',
+  },
 })
